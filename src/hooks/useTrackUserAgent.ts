887700e2114fef9cf767edd8f58407fce01e347c
Luis <simosa37@gmail.com>
2023-11-29 15:02:41 -0300

    deleted useRouter import in useTrackUserAgent hook

--- src/hooks/useTrackUserAgent.ts
+++ src/hooks/useTrackUserAgent.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import UAParser from "ua-parser-js";
 
